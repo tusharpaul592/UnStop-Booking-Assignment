97b92fc85b98d835faa860d25cf313c53b0a1cd0
Tushar <tushar.2@girnarsoft.co.in>
2023-07-16 15:00:54 +0530

    INITIAL COMMIT

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with the URL of your frontend
+    methods: ['GET', 'POST'],
+    optionsSuccessStatus: 200,
+  };
+  
+  app.use(cors(corsOptions));
+// app.use(cors());
+
+// Available seats in the coach
+const totalRows = 11;
+const seatsPerRow = 7;
+const lastRowSeats = 3;
+
+const totalSeats = (totalRows - 1) * seatsPerRow + lastRowSeats;
+
+const bookedSeats = new Set(); // Set to store booked seat numbers
+
+// Endpoint to fetch all seats and their availability status
+app.get('/api/seats', (req, res) => {
+  const seats = [];
+  for (let row = 1; row <= totalRows; row++) {
+    const seatsInRow = row === totalRows ? lastRowSeats : seatsPerRow;
+    for (let seat = 1; seat <= seatsInRow; seat++) {
+      const seatNumber = (row - 1) * seatsPerRow + seat;
+      const isBooked = bookedSeats.has(seatNumber);
+      seats.push({ seatId: seatNumber, rowNumber: row, seatNumber: seat, isBooked });
+    }
+  }
+  res.json(seats);
+});
+
+
+// Endpoint to book seats
+app.post('/api/seats/book', (req, res) => {
+  const { seatCount } = req.body;
+
+  if (seatCount <= 0 || seatCount > seatsPerRow) {
+    return res.status(400).json({ error: 'Invalid number of seats requested' });
+  }
+
+  const booked = bookSeats(seatCount);
+  if (booked.length === 0) {
+    return res.status(404).json({ error: 'No available seats' });
+  }
+
+  return res.json({ seats: booked });
+});
+
+// Helper function to book seats
+function bookSeats(count) {
+  const booked = [];
+
+  for (let row = 1; row <= totalRows; row++) {
+    const seatsInRow = row === totalRows ? lastRowSeats : seatsPerRow;
+    let consecutiveCount = 0;
+    let startSeat = -1;
+
+    for (let seat = 1; seat <= seatsInRow; seat++) {
+      const seatNumber = (row - 1) * seatsPerRow + seat;
+      if (bookedSeats.has(seatNumber)) {
+        consecutiveCount = 0;
+        startSeat = -1;
+      } else {
+        if (consecutiveCount === 0) {
+          startSeat = seat;
+        }
+        consecutiveCount++;
+        if (consecutiveCount === count) {
+          for (let i = 0; i < count; i++) {
+            const bookedSeat = startSeat + i;
+            bookedSeats.add((row - 1) * seatsPerRow + bookedSeat);
+            booked.push({ seatId: bookedSeat, rowNumber: row, seatNumber: bookedSeat });
+          }
+          return booked;
+        }
+      }
+    }
+  }
+
+  return booked;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [seats, setSeats] = useState([]);
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [message, setMessage] = useState('');
+  const [requiredSeats, setRequiredSeats] = useState(1);
+  const [isFetchingSeats, setIsFetchingSeats] = useState(false);
+  const [isBooking, setIsBooking] = useState(false);
+
+  const fetchSeats = async () => {
+    try {
+      setIsFetchingSeats(true);
+      const response = await axios.get('http://localhost:3001/api/seats');
+      setSeats(response.data);
+      setIsFetchingSeats(false);
+    } catch (error) {
+      console.error(error);
+      setIsFetchingSeats(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchSeats();
+  }, []);
+
+  useEffect(() => {
+    const updateSelectedSeats = () => {
+      setSelectedSeats((prevSelectedSeats) => prevSelectedSeats.slice(0, requiredSeats));
+    };
+
+    updateSelectedSeats();
+  }, [requiredSeats]);
+
+  const handleSeatClick = (seatId) => {
+    setSelectedSeats((prevSelectedSeats) => {
+      if (prevSelectedSeats.includes(seatId)) {
+        return prevSelectedSeats.filter((id) => id !== seatId);
+      } else {
+        if (prevSelectedSeats.length < requiredSeats) {
+          return [...prevSelectedSeats, seatId];
+        }
+        return prevSelectedSeats;
+      }
+    });
+  };
+
+  const handleBookSeats = async () => {
+    if (requiredSeats > 7) {
+      alert("You can only book a maximum of 7 seats.")
+      setMessage('You can only book a maximum of 7 seats.');
+      return;
+    }
+
+    try {
+      setIsBooking(true);
+      const bookedCount = seats.filter((seat) => seat.isBooked).length;
+      const availableSeatsCount = 80 - bookedCount;
+  
+      if (availableSeatsCount === 0) {
+        setMessage('All seats are booked. No more seats available.');
+        setIsBooking(false);
+        return;
+      }
+  
+      if (requiredSeats > availableSeatsCount) {
+        setMessage(`Only ${availableSeatsCount} seat(s) available. Please choose a lower number of seats.`);
+        setIsBooking(false);
+        return;
+      }
+
+      const response = await axios.post('http://localhost:3001/api/seats/book', { seatCount: requiredSeats });
+      console.log('response',response)
+      const { seats: bookedSeats } = response.data;
+      const seatNumbers = bookedSeats.map((seat) => seat.seatId);
+      setMessage(`Seats booked: ${seatNumbers.join(', ')}`);
+      setSelectedSeats([]);
+      setIsBooking(false);
+
+      fetchSeats();
+    } catch (error) {
+      console.error(error);
+      setMessage('Error occurred while booking seats');
+      setIsBooking(false);
+    }
+  
+  };
+
+  const handleRequiredSeatsChange = (event) => {
+    setRequiredSeats(parseInt(event.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Seat Booking</h1>
+      <p className="message">{message}</p>
+      <div className="seats-container">
+        {isFetchingSeats ? (
+          <p>Loading seats...</p>
+        ) : (
+          seats.map((seat) => (
+            <div
+              key={seat.seatId}
+              className={`seat ${seat.isBooked ? 'booked' : selectedSeats.includes(seat.seatId) ? 'selected' : 'available'}`}
+              onClick={() => handleSeatClick(seat.seatId)}
+            >
+              <span className="seat-label">Seat {seat.rowNumber}-{seat.seatNumber}</span>
+            </div>
+          ))
+        )}
+      </div>
+      <div className="input-container">
+        <label htmlFor="requiredSeats">{`Number of Seats:                     `}</label>
+        <input
+          type="number"
+          id="requiredSeats"
+          name="requiredSeats"
+          min="1"
+          value={requiredSeats}
+          onChange={handleRequiredSeatsChange}
+        />
+      </div>
+      <button
+  className="book-btn"
+  onClick={handleBookSeats}
+  disabled={isBooking || seats.filter((seat) => !seat.isBooked).length === 0}
+>
+  {isBooking ? 'Booking...' : `Book ${!requiredSeats?"0":requiredSeats} Seat${requiredSeats !== 1 ? 's' : ''}`}
+</button>
+
     </div>
   );
-}
+};
 
 export default App;
